f14f3c81e71a67d40f892eddb67c3ac185636679
vikalp-tomer <vikalptomar15@gmail.com>
2022-02-15 13:41:42 +0530

    backend-9th-video-delete-and-like-dislike-a-post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -28,4 +28,35 @@ router.put("/:id", async (req, res)=>{
   }
 })
 
+//delete a post
+router.delete("/:id", async (req, res)=>{
+ try{
+  const post = await Post.findById(req.params.id)
+  if(post.userId === req.body.userId){
+    await post.deleteOne();
+    res.status(200).json("the post has been deleted")
+  } else {
+    res.status(403).json("You can delete only your post")
+  }
+ } catch(err){
+   res.status(500).json(err)
+ }
+})
+
+//like and dislike a post
+router.put("/:id/like", async(req, res)=>{
+  try{
+    const post = await Post.findById(req.params.id)
+    if(!post.likes.includes(req.body.userId)){
+      await post.updateOne({$push:{likes:req.body.userId}})
+      res.status(200).json("the post has been liked")
+    } else {
+      await post.updateOne({$pull:{likes:req.body.userId}})
+      res.status(200).json("the post has been disliked")
+    }
+  } catch(err){
+    res.status(500).json(err)
+  }
+})
+
 module.exports = router;
